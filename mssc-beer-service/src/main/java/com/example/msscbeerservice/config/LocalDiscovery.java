958c3d4583210bb8c489fe232671e43f4a2722c7
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2024-06-16 15:10:51 +0530

    added spring cloud eureka client

---
+++ mssc-beer-service/src/main/java/com/example/msscbeerservice/config/LocalDiscovery.java
@@ -0,0 +1,12 @@
+package com.example.msscbeerservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile(value = {"local"})
+@Configuration
+@EnableDiscoveryClient
+public class LocalDiscovery {
+
+}
